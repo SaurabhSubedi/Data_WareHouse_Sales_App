b7ba993158cb50d7c1c9f5a4aeb6facd4a3d2ff7
SaurabhSubedi <67773187+SaurabhSubedi@users.noreply.github.com>
2026-02-09 12:55:44 +0545

    Update silver_loader.sql

--- scripts/silver/silver_loader.sql
+++ scripts/silver/silver_loader.sql
@@ -65,3 +65,46 @@ SELECT
 FROM bronze.crm_prd_info;
 
 
+TRUNCATE TABLE silver.crm_sales_details;
+
+INSERT INTO silver.crm_sales_details(sls_ord_num,sls_prd_key,sls_cust_id,sls_order_dt,sls_ship_dt,sls_due_dt,sls_sales,sls_quantity,sls_price)
+select sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+(
+case 
+when (sls_order_dt = 0 OR LENGTH(sls_order_dt :: text) != 8) then NULL
+else cast(sls_order_dt :: text as date) 
+end
+) as sls_order_dt,
+(
+case 
+when (sls_ship_dt = 0 OR LENGTH(sls_ship_dt :: text) != 8) then NULL
+else cast(sls_ship_dt :: text as date) 
+end
+) as sls_ship_dt,
+(
+case 
+when (sls_due_dt = 0 OR LENGTH(sls_due_dt :: text) != 8) then NULL
+else cast(sls_due_dt :: text as date) 
+end
+) as sls_due_dt,
+(
+case when (sls_sales is null) or (sls_sales <=0) or (sls_sales != sls_quantity * abs(sls_price))
+then sls_quantity * abs(sls_price) 
+else 
+sls_sales
+end
+) as sls_sales,
+sls_quantity,
+(
+case when (sls_price is null) or (sls_price <=0)
+then sls_sales / NULLIF(sls_quantity,0)
+else 
+sls_price
+end
+) as sls_price
+from bronze.crm_sales_details;
+
+
+
